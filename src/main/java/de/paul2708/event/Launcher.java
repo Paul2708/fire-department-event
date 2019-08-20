17dde0ba2476a6f9a5a475479ae3621f7c6dde40
Paul2708 <paul.hoger@gmx.net>
2019-08-20 13:26:25 +0200

    Launch the view in main class

--- src/main/java/de/paul2708/event/Launcher.java
+++ src/main/java/de/paul2708/event/Launcher.java
@@ -1,5 +1,7 @@
 package de.paul2708.event;
 
+import de.paul2708.event.view.ApplicationView;
+
 /**
  * This class holds the main method and runs the application.
  *
@@ -8,11 +10,20 @@ package de.paul2708.event;
 public final class Launcher {
 
     /**
+     * Nothing to call here.
+     */
+    private Launcher() {
+        throw new IllegalAccessError();
+    }
+
+    /**
      * Program entry. Launches the view.
      *
      * @param args ignored command line arguments
      */
     public static void main(String[] args) {
-        // TODO: Implement me
+        ApplicationView view = ApplicationView.by();
+
+        view.launch();
     }
 }
