536b2571b8b2a330823d74f2c58cbaa4f411cadc
Paul2708 <paul.hoger@gmx.net>
2019-08-20 13:16:39 +0200

    Added launcher class

---
+++ src/main/java/de/paul2708/event/Launcher.java
@@ -0,0 +1,18 @@
+package de.paul2708.event;
+
+/**
+ * This class holds the main method and runs the application.
+ *
+ * @author Paul2708
+ */
+public final class Launcher {
+
+    /**
+     * Program entry. Launches the view.
+     *
+     * @param args ignored command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO: Implement me
+    }
+}
